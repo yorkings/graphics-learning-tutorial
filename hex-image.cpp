17db656f4eef2cbdf92bc422fdccac700a54a42f
yorkings <yorkingsnyakundi2700@gmail.com>
2026-06-14 17:33:38 +0300

    color management

---
+++ hex-image.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+
+int main() {
+    constexpr int nx = 1920;
+    constexpr int ny = 1080;
+    
+    // Open file in BINARY mode
+    std::ofstream image("image.ppm", std::ios::binary);
+    
+    // Write Header (P6 instead of P3)
+    image << "P6\n" << nx << " " << ny << "\n255\n";
+
+    for (int j = ny - 1; j >= 0; --j) {
+        for (int i = 0; i < nx; ++i) {
+            // Calculate colors as before
+            unsigned char r = static_cast<unsigned char>(255.999 * i / (nx - 1));
+            unsigned char g = static_cast<unsigned char>(255.999 * j / (ny - 1));
+            unsigned char b = static_cast<unsigned char>(255.999 * 0.25);            
+            // Write raw bytes directly (no spaces, no newlines between pixels)
+            image << r << g << b;
+        }
+    }
+    return 0;
+}   
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 ## computer graphics
 ---
+*yorkings*
 
+#### introduction
+i am new to computer graphics but bear with me in my path,i am as you just continous learning despite the current AI regime.
+
+### first test
+
+there are two ways we used both p6 and p3
